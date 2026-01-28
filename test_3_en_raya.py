582288fa8eceb6a4bb3cfebf3ee8cc65c46b71ed
Irene Zamora Pérez <irene.zperez@edu.upct.es>
2026-01-28 20:00:04 +0100

    Añadiendo fichero con los test de juego_3_en_raya.py

---
+++ test_3_en_raya.py
@@ -0,0 +1,52 @@
+# implementación de los test unitaros para el juego 3 en raya
+
+from juego_3_en_raya import generar_tablero, movimiento_valido, jugada_ganadora    
+import os as os
+
+# testeamos que el tablero se genera correctamente con las dimensiones adecuadas 
+def test_generar_tablero():  
+
+    mov_jugador_1 = {}   
+
+    mov_jugador_2 = {}   
+
+    movimientos_jugadores=[mov_jugador_1, mov_jugador_2]   
+
+    n=3  
+
+    t= generar_tablero(n, movimientos_jugadores)
+
+    assert len(t)== n  
+
+    for f in t:  
+        assert len(f) == n 
+
+# testeamos si un movimiento es válido mendiante la función movimiento_valido
+def test_movimiento_columna_fuera_tablero():
+    n = 3 
+    movimientos_otro_jugador={} 
+    x=  1 
+    y=  n+1 
+    assert False == movimiento_valido(x,y,movimientos_otro_jugador) 
+
+def test_movimiento_fila_y_columna_fuera_tablero(): 
+    n = 3
+    movimientos_otro_jugador={} 
+    x=  n+1 
+    y=  n+1 
+    assert False == movimiento_valido(x,y,movimientos_otro_jugador) 
+ 
+def test_movimiento_incorrecto(): 
+    movimientos_otro_jugador={2:[3]} 
+    x=  2 
+    y=  3 
+    assert False == movimiento_valido(x,y,movimientos_otro_jugador)
+
+# testeamos si una jugada es ganadora mediante la función jugada_ganadora
+def test_no_ganador(): 
+    movimientos_jugador={2:[2,3]} 
+    assert False == jugada_ganadora(movimientos_jugador) 
+ 
+def test_ganador(): 
+    movimientos_jugador={2:[1,2,3]} # creamos diccionario que representa los movimientos del jugador
+    assert True == jugada_ganadora(movimientos_jugador) # esta línea comprueba que la funcion devuelve True porque el jugador tiene 3 fichas en la misma fila
