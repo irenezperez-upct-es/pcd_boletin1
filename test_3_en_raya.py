920a07dec455414ae48a93260338b80d88f4713d
Irene Zamora Pérez <irene.zperez@edu.upct.es>
2026-01-28 21:12:19 +0100

    Se añaden test de movimiento ganador para que detecten además de la fila la columna y la diagonas. Además se añade un test de movimiento no ganador para que se detecte que aunque en una misma fila o columna hay 3 fichas, si no van seguidas no son válidas

--- test_3_en_raya.py
+++ test_3_en_raya.py
@@ -43,10 +43,38 @@ def test_movimiento_incorrecto():
     assert False == movimiento_valido(x,y,movimientos_otro_jugador)
 
 # testeamos si una jugada es ganadora mediante la función jugada_ganadora
-def test_no_ganador(): 
+def test_no_ganador_falta_casilla(): 
     movimientos_jugador={2:[2,3]} 
     assert False == jugada_ganadora(movimientos_jugador) 
+
+def test_no_ganador_movimientos():
+    # movimientos dispersos que no forman la línea
+    movimientos_jugador = {
+        1: [1],
+        2: [3],
+        3: [2]
+    }
+    assert False == jugada_ganadora(movimientos_jugador) 
  
-def test_ganador(): 
+def test_ganador_fila(): 
+    # el jugador ocupa toda la fila 2
     movimientos_jugador={2:[1,2,3]} # creamos diccionario que representa los movimientos del jugador
     assert True == jugada_ganadora(movimientos_jugador) # esta línea comprueba que la funcion devuelve True porque el jugador tiene 3 fichas en la misma fila
+
+def test_ganador_columna():
+    # el jugador ocupa la columna 1 en las filas 1, 2, 3
+    movimientos_jugador = {
+        1: [1],
+        2: [1],
+        3: [1]
+    }
+    assert True == jugada_ganadora(movimientos_jugador)
+
+def test_ganador_diagonal():
+    # el jugador ocupa (1,1), (2,2), (3,3)
+    movimientos_jugador = {
+        1: [1],
+        2: [2],
+        3: [3]
+    }
+    assert True == jugada_ganadora(movimientos_jugador)
